6e5ba6d96c5ce8bc39935445b353d539fa25903e
cjglo <chris.j.gallo3@gmail.com>
2024-04-01 17:43:04 -0500

    start

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,44 @@
-#include "../raylib/include/raylib.h"
+#include <raylib.h>
 #include <memory>
 
 #include "WindowConstants.hpp"
 #include "parser/TextHandler.hpp"
 #include "file_system/FileEditor.hpp"
 
+using namespace windowConstants;
+
 int main(int argc, char* argv[]) {
 
-    auto fileName = std::string{"defaultFileName.txt"};
-    if(argc > 0) {
-        fileName = std::string{argv[0]};
-    }
-    auto fileEditor = file_system::FileEditor{fileName};
-    if(!fileEditor.initFile()) {
-        // TODO add log
-        return 1;
-    }
+    /* Code left from when IDE, will use in future, but not currently essential
+     *
+        auto fileName = std::string{"defaultFileName.txt"};
+        if(argc > 0) {
+            fileName = std::string{argv[0]};
+        }
+        auto fileEditor = file_system::FileEditor{fileName};
+        if(!fileEditor.initFile()) {
+            // TODO add log
+            return 1;
+        }
 
-    InitWindow(windowConstants::WINDOW_LENGTH, windowConstants::WINDOW_HEIGHT, windowConstants::title);
+     */
+
+    InitWindow(WINDOW_LENGTH, WINDOW_HEIGHT, title);
     SetTargetFPS(100);
     auto textHandler = std::make_unique<parser::TextHandler>(parser::TextHandlerFlags{});
-    auto text = fileEditor.readFile();
-    auto cursor = std::pair<int32_t , int32_t>{10,10};
+    auto text = std::string{};
+    auto cursor = std::pair<int32_t , int32_t>{CURSOR_START_X,CURSOR_START_Y};
 
 
     while(!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(WHITE);
+        ClearBackground(BLACK);
         text = textHandler->readKeyboard();
-        DrawText(text.c_str(), cursor.first, cursor.second, 14, BLACK);
+        auto textConsoleStart = CURSOR_START_Y-DEFAULT_WNINDOW_SPACING;
+        DrawLine(0, textConsoleStart, WINDOW_LENGTH, textConsoleStart, WHITE);
+        DrawText(text.c_str(), cursor.first, cursor.second, 14, WHITE);
 
         EndDrawing();
-    }
-
-    if(!fileEditor.writeFile(text)) {
-        // TODO add severe log and dump contents
     }
 
     return 0;
---
+++ src/world/WorldGenerator.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+class WorldGenerator {
+
+    WorldGenerator();
+
+};
+
+
